a81e8d7032439c370d79c896129a56e1df24e229
edumats <eduardo.mats@gmail.com>
2021-03-08 19:39:15 -0300

    Testing github actions

---
+++ posts/test_page.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+
+class TestPostsPages(StaticLiveServerTestCase):
+
+    def  test_post_creation(self):
+        pass
