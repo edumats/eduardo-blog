e2cfb7c7f330ef03c10084d932646ecb401f239f
edumats <eduardo.mats@gmail.com>
2021-03-09 15:38:22 -0300

    Option to switch between dbs

--- blog/settings.py
+++ blog/settings.py
@@ -70,17 +70,29 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('SQL_ENGINE'),
-        'NAME': os.getenv('SQL_DATABASE'),
-        'USER': os.getenv('SQL_USER'),
-        'PASSWORD': os.getenv('SQL_PASSWORD'),
-        'HOST': os.getenv('SQL_HOST'),
-        'PORT': os.getenv('SQL_PORT'),
+# Different database configurations, if on github or other environment
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('SQL_DATABASE'),
+            'USER': os.getenv('SQL_USER'),
+            'PASSWORD': os.getenv('SQL_PASSWORD'),
+            'HOST': os.getenv('SQL_HOST'),
+            'PORT': os.getenv('SQL_PORT'),
+        }
+    }
 
 
 # Password validation
